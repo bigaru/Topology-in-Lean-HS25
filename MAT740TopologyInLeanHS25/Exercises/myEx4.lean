9c708c3f8f6aea8c441a070c40b2727dd1a95552
Arooran Thanabalasingam <13069024+bigaru@users.noreply.github.com>
2025-10-20 15:08:45 +0200

    update myEx4 according to imports

--- MAT740TopologyInLeanHS25/Exercises/myEx4.lean
+++ MAT740TopologyInLeanHS25/Exercises/myEx4.lean
@@ -5,7 +5,7 @@ import MAT740TopologyInLeanHS25.Definitions.ContinuousFunctions
 universe u v
 variable {X : Type u} {Y : Type v} (f : X → Y) (g : Y → X)
 
-lemma l1 (inv_fg : InverseFun f g) (x : X) : g (f x) = x := by
+lemma l1' (inv_fg : InverseFun f g) (x : X) : g (f x) = x := by
   rw [InverseFun] at inv_fg
   obtain ⟨h1, h2⟩ := inv_fg
 
@@ -13,7 +13,7 @@ lemma l1 (inv_fg : InverseFun f g) (x : X) : g (f x) = x := by
   simp only [Function.comp_apply, id_eq] at h2
   exact h2 x
 
-lemma l2 (inv_fg : InverseFun f g) (y : Y) : f (g y) = y := by
+lemma l2' (inv_fg : InverseFun f g) (y : Y) : f (g y) = y := by
   rw [InverseFun] at inv_fg
   obtain ⟨h1, h2⟩ := inv_fg
 
@@ -21,18 +21,18 @@ lemma l2 (inv_fg : InverseFun f g) (y : Y) : f (g y) = y := by
   simp only [Function.comp_apply, id_eq] at h1
   exact h1 y
 
-lemma image_eq_preimage_InverseFun (inv_fg : InverseFun f g) (U : Set X) : f '' U = g ⁻¹' U := by
+lemma image_eq_preimage_InverseFun' (inv_fg : InverseFun f g) (U : Set X) : f '' U = g ⁻¹' U := by
   ext y
   constructor
   · intro hf
     rcases hf with ⟨x, hxU, rfl⟩
     rw [Set.mem_preimage]
-    rw [l1 f g inv_fg x]
+    rw [l1' f g inv_fg x]
     exact hxU
 
   · intro hg
     use g y
-    rw [l2 f g inv_fg y]
+    rw [l2' f g inv_fg y]
     constructor
     · rw [Set.mem_preimage] at hg
       exact hg
@@ -44,7 +44,7 @@ example : HomeoMap f → OpenMap f := by
   intro h
   rcases h.2 with ⟨g, gCont, inv_fg⟩
   intro U hU
-  rw [image_eq_preimage_InverseFun f g inv_fg U]
+  rw [image_eq_preimage_InverseFun' f g inv_fg U]
   apply gCont
   exact hU
 
@@ -56,7 +56,7 @@ example (inv_fg : InverseFun f g) (cont_f : Cont f) : OpenMap f → HomeoMap f :
     constructor
     · rw [Cont]
       intro U hU
-      rw [← image_eq_preimage_InverseFun f g inv_fg U]
+      rw [← image_eq_preimage_InverseFun' f g inv_fg U]
       exact h U hU
     · exact inv_fg
 
