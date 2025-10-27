cb77c331b8c545d3a8cd1ed7995e034499410efb
Arooran Thanabalasingam <13069024+bigaru@users.noreply.github.com>
2025-10-27 09:34:40 +0100

    add myEx5 problem 2

--- MAT740TopologyInLeanHS25/Exercises/myEx5.lean
+++ MAT740TopologyInLeanHS25/Exercises/myEx5.lean
@@ -9,12 +9,12 @@ variable {X : Type u} {Y : Type v}
 section Problem1
 variable [Topology X] [BY : Basis Y]
 
+/-
+  (=>) use `Open_Basics`
+  (<=) use `IsBasis_basisTopology` to decompose open U into union of basis element.
+  Then use properties of preimages `Set.preimage_sUnion` and `Set.sUnion_image`.
+-/
 theorem Cont_Basics' (f : X → Y) : Cont f ↔ ∀ b ∈ BY.Basics, Open (f ⁻¹' b) := by
-  /-
-    (=>) use `Open_Basics`
-    (<=) use `IsBasis_basisTopology` to decompose open U into union of basis element.
-    Then use properties of preimages `Set.preimage_sUnion` and `Set.sUnion_image`.
-  -/
   constructor
   · intro hf b hb
     apply hf b
@@ -42,21 +42,46 @@ variable [MetricSpace X]
 
 
 /- (a) (3 points) -/
-theorem ball_in_ball' {x : X} {ε : ℝ} : ∀ y ∈ ball x ε, ∃ δ, (0 < δ ∧ ball y δ ⊆ ball x ε) := by
-  sorry
-  /-
+/-
   use `linarith` and `dist_triangle`
-  -/
+-/
+theorem ball_in_ball' {x : X} {ε : ℝ} : ∀ y ∈ ball x ε, ∃ δ, (0 < δ ∧ ball y δ ⊆ ball x ε) := by
+  intro y hy
+  let δ := ε - dist x y
+  rw [mem_ball, dist_comm] at hy
+
+  use δ
+  constructor
+  · rw [← sub_pos] at hy
+    exact hy
+
+  · intro z hz
+    rw [mem_ball, dist_comm] at hz
+    rw [mem_ball, dist_comm]
+
+    have htriangle: dist x z ≤ dist x y + dist y z := dist_triangle x y z
+    linarith
 
 /- (b) (3 points) -/
 instance metricBasis' : Basis X where
   Basics := {B | ∃ x ε, B = ball x ε}
   Basis_cover := by
-    sorry
     /-
     Prove this!
     use `simp?` here to get a short proof
     -/
+    ext y
+    constructor
+    · intro hy
+      simp only [Set.mem_univ]
+
+    · intro h1
+      simp only [Set.mem_sUnion, Set.mem_setOf_eq]
+      use ball y 1
+      constructor
+      · use y, 1
+      · simp only [mem_ball, dist_self, zero_lt_one]
+
   Basis_inter := by
     /- No need to prove this! -/
     sorry
