2b66901ee05231506b15c8f7efba9cdd153f0a5d
Arooran Thanabalasingam <13069024+bigaru@users.noreply.github.com>
2025-11-23 11:59:26 +0100

    add Ex9

--- MAT740TopologyInLeanHS25/Exercises/Ex9.lean
+++ MAT740TopologyInLeanHS25/Exercises/Ex9.lean
@@ -10,16 +10,39 @@ theorem Disconnected_Prop : ¬(Connected Prop) := by
   push_neg
   have cont_id : Cont (id : Prop → Prop) := Cont_id
   have non_const : ∀ b : Prop, ¬(Constant id b) := by
-    sorry -- exercise
+    intro b hb
+    have eqF : False = b := by
+      simpa [id] using hb False
+    have eqT : True = b := by
+      simpa [id] using hb True
+    let e := eqF.trans eqT.symm
+    contradiction
   use id
 
 theorem Connected_image (f : X → Y) (surj_f : Function.Surjective f) (cont_f : Cont f)
-  : Connected X → Connected Y := by
-    sorry -- exercise
+: Connected X → Connected Y := by
+    intro hX hY cont_hY
+    have cont_hYf : Cont (hY ∘ f) := Cont_comp f hY cont_f cont_hY
+    obtain ⟨b, const_hYf⟩ := hX (hY ∘ f) cont_hYf
+    use b
+    intro y
+    obtain ⟨x, hx⟩ := surj_f y
+    have hfinal : hY (f x) = b := const_hYf x
+    rw [hx] at hfinal
+    exact hfinal
+
 
 theorem PathConnected_image (f : X → Y) (surj_f : Function.Surjective f) (cont_f : Cont f)
   : PathConnected X → PathConnected Y := by
-    sorry -- exercise
-    /- Hints:
-      - Use `Classical.choice` the get a witness of `Nonempty _`.
-      - Use `constructor` to provide witness for `Nonempty _` -/
+    intro hP y1 y2
+    obtain ⟨x1, hx1⟩ := surj_f y1
+    obtain ⟨x2, hx2⟩ := surj_f y2
+    have np : Nonempty (Path x1 x2) := hP x1 x2
+    let p := Classical.choice np
+    let mp := mapPath f cont_f p
+    let pathY : Path y1 y2 :=
+      { p := mp.p
+        Cont_p := mp.Cont_p
+        source := mp.source.trans hx1
+        target := mp.target.trans hx2 }
+    exact ⟨pathY⟩
